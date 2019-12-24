ac1480032b6829dcb602fa5f9622864fcc65a766
Dan Nissim <nissim.dan@gmail.com>
2019-12-24 02:20:26 +0200

    added comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
+import { DiscussionEmbed } from "disqus-react"
 
 import Bio from '../components/Bio'
 import { rhythm, scale } from '../utils/typography'
@@ -11,6 +12,10 @@ class BlogPostTemplate extends React.Component {
     const post = this.props.data.markdownRemark
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
     const { previous, next } = this.props.pathContext
+    const disqusConfig = {
+      shortname: 'danrambles',
+      config: { identifier: post.frontmatter.title },
+    }
 
     return (
       <div>
@@ -59,6 +64,7 @@ class BlogPostTemplate extends React.Component {
             </li>
           )}
         </ul>
+        <DiscussionEmbed {...disqusConfig} />
       </div>
     )
   }
